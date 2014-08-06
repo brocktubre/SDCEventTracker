f7732d2da48118ecd1cd572b6c4d300aa90ec122
brock <brocktubre@gmail.com>
2014-08-06 09:20:40 -0500

    Intital Push
    First push of MVC SDC Event Tracker

---
+++ Database Files/InitialQuery.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Event(
+	ID int PRIMARY KEY NOT NULL identity(1,1),
+	EventName varchar(128),
+	Date DATETIME,
+	Location varchar(128),
+	City varchar(128),
+	State varchar(128),
+	Zip int,
+	MorningHunt bit DEFAULT(0),
+	EveningHunt bit DEFAULT(0),
+	BenchShow bit DEFAULT(0), 
+	BarkingContest bit DEFAULT(0)
+
+)
+
+CREATE TABLE Handler(
+	ID int PRIMARY KEY NOT NULL identity (1000, 1),
+	FristName varchar(45),
+	LastName varchar(45),
+
+)
+
+CREATE TABLE Dog(
+	ID int PRIMARY KEY NOT NULL identity (1,1),
+	Name varchar(128),
+	Handler int FOREIGN KEY REFERENCES Handler(ID)
+)
+
+
+CREATE TABLE Result(
+	Event int, /* Defines the exact Overall Event  */
+	EventType int, /* 1-4 Defines the exact Event Type (morning hunt, evening hunt, bench show, barking contest)  */
+	Place int, 
+	Handler int,
+	Dog int, 
+)
+
+INSERT INTO Event (EventName, Location, Date, City, State, Zip, MorningHunt, EveningHunt, BenchShow, BarkingContest) VALUES ('TN State Hunt', 'Buffalo River Mangm. Area', '02/02/1994', 'Columbia', 'Tennesse', 30678, 1, 1, 0, 1) 
+INSERT INTO Event (EventName, Location, City, State, Zip, MorningHunt, EveningHunt, BenchShow, BarkingContest) VALUES ('Franklin County Hunt', 'Ed Hardy Woods', 'Franklin', 'Tennesse', 30009, 1, 1, 0, 0)   
---
+++ SDCEventTracker/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using SDCEventTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace SDCEventTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public SDC_databaseEntities db = new SDC_databaseEntities();
+        
+
+        public ActionResult Competitions(string searchString)
+        {
+            ViewBag.Title = "Competitions";
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                return View(db.Events.Where(item => item.EventName.Contains(searchString)));
+            }
+            else
+            {
+               return View(db.Events.ToList());
+            }
+            //List<db> EventList = new List<db>;
+            //var query = from item in db.Events where item.ID == 1 select item;
+            //var theEvent = query.FirstOrDefault();
+            //theEvent.EventName = "Shit Stain Event";
+            //db.SaveChanges(); 
+            //var query = from item in db.Events where item.ID >= 0 select item;
+            //var theEvent = query.FirstOrDefault();            
+        }
+
+        [HttpGet]
+        public ActionResult Create(string searchString)
+        {
+            ViewBag.Title = "Submit New Hunt";
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EventName,Date,Location,City,State,Zip,MorningHunt,EveningHunt,BenchShow,BarkingContest")] Event EventToCreate)
+        {
+
+            if (ModelState.IsValid && EventToCreate.State != string.Empty)
+            {  
+                db.Events.Add(@EventToCreate);
+                db.SaveChanges();
+                return RedirectToAction("Competitions");
+            }
+            else
+            {
+                return View(@EventToCreate);
+            }
+        }
+    }
+}
