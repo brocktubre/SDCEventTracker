440c3e9012dabeb9f8eff29d112d87441a643fc0
Brock Tubre <brocktubre@gmail.com>
2014-08-24 13:46:46 -0500

    Comment sections

--- SDCEventTracker/Controllers/HomeController.cs
+++ SDCEventTracker/Controllers/HomeController.cs
@@ -13,32 +13,35 @@ namespace SDCEventTracker.Controllers
     {
         public SDC_databaseEntities db = new SDC_databaseEntities();
 
+        // Competitions View produces all the Events, past and present
         public ActionResult Competitions()
         {
             // results stores the linq query that produces all the Events in descending order
-            var results = from i in db.Events orderby i.Date descending select i;
+            var allEvents = from i in db.Events orderby i.Date descending select i;
             ViewBag.Title = "Competitions";
-            return View(results.ToList());  
+            return View(allEvents.ToList());// returns the list of all the events  
         }
 
+        // HttpGet is used to search through table for specific hunt using searchString
+        // allEvents is used if the searchString is null
         [HttpGet]
         public ActionResult Competitions(string searchString)
         {
-
             ViewBag.Title = "Competitions";
-            var results = from i in db.Events orderby i.Date descending select i;
+            var allEvents = from i in db.Events orderby i.Date descending select i; 
             if (!String.IsNullOrEmpty(searchString))
             {
                 return View(db.Events.OrderByDescending(item => item.Date).Where(item => item.EventName.Contains(searchString)));
             }
             else
             {
-                return View(results.ToList());
+                return View(allEvents.ToList());
             }
 
         }
 
-        [Authorize]
+        // HttpGet is used to submit upcoming event, date cannot be in the past
+        [Authorize] //User must be logged in
         [HttpGet]
         public ActionResult SubmitEvent(string searchString)
         {
@@ -46,11 +49,13 @@ namespace SDCEventTracker.Controllers
             return View();
         }
 
+        // HttpPost is used to submit upcoming hunt and make sure all fields are correct. 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult SubmitEvent([Bind(Include = "EventName,Date,Location,City,State,Zip,MorningHunt,EveningHunt,BenchShow,BarkingContest")] Event EventToCreate)
         {
             ViewBag.Title = "Submit New Hunt";
+            // if statement passes if Valid and state string has a value
             if (ModelState.IsValid && EventToCreate.State != string.Empty)
             {
                 db.Events.Add(@EventToCreate);
@@ -63,6 +68,7 @@ namespace SDCEventTracker.Controllers
             }
         }
 
+        // HttpGet is used to get the results of a past hunt. The EventID is passed in as '/Home/EventDetails?id=EventID'
         [HttpGet]
         public ActionResult EventDetails(int? id)
         {
@@ -71,6 +77,7 @@ namespace SDCEventTracker.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+            // .Find(id) finds the EventId and stores is into @event
             Event @event = db.Events.Find(id);
             if(@event == null)
             {
@@ -79,6 +86,7 @@ namespace SDCEventTracker.Controllers
             return View(@event);
         }
 
+        // HttpGet is used to show the Event Results: Place, Handler's Name, and Dog's Name.
         [HttpGet]
         public ActionResult EventResults(int? id)
         {
